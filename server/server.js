6a4919e209858ce01a411c6cadf04954ab0dbd18
yusufkoroglu28 <yusuf_travego_yusufkoroglu@hotmail.com>
2025-06-15 16:11:23 +0300

    fix: remove app.listen for Vercel compatibility

--- server/server.js
+++ server/server.js
@@ -22,4 +22,6 @@ app.get('/',(req,res)=>res.send('API is working'))
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>console.log(`Server running on port ${PORT}`));
+//app.listen(PORT,()=>console.log(`Server running on port ${PORT}`));
+
+export default app
